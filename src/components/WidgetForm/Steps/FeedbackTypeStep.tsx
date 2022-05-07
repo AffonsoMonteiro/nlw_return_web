b70dd8c232c19dab9fdeff2f7136af5c388b9b75
AffonsoMonteiro <afonsomonteiro_@hotmail.com>
2022-05-07 13:59:23 -0300

    web finished

--- src/components/WidgetForm/Steps/FeedbackTypeStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackTypeStep.tsx
@@ -1,4 +1,5 @@
 import { FeedbackType, feedbackTypes } from "..";
+import { CloseButton } from "../../CloseButton";
 
 interface FeedbackTypeStepProps {
   onFeedbackTypeChange: (type: FeedbackType) => void;
@@ -6,6 +7,13 @@ interface FeedbackTypeStepProps {
 
 export function FeedbackTypeStep({onFeedbackTypeChange}:FeedbackTypeStepProps) {
   return (
+    <>
+      <header>
+        <span className="text-xl leading-6">Deixe seu feedback</span>
+
+        <CloseButton />
+      </header>
+
     <div className="flex py-8 gap-2 w-full">
       {Object.entries(feedbackTypes).map(([key, value]) => (
         <button
@@ -19,5 +27,6 @@ export function FeedbackTypeStep({onFeedbackTypeChange}:FeedbackTypeStepProps) {
         </button>
       ))}
     </div>
+    </>
   )
 }
